a332b90b56f058113ac53425e266c44b5ba527eb
Thar Nyi Phyo <konyi.tharnyiphyo@gmail.com>
2025-01-26 19:41:29 +0630

    update init

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons'],
-    },
-  },
+      // Remove external modules so that they are bundled
+      // external: ['@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons']
+    }
+  }
 });
